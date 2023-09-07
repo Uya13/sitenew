f6706964c7f9360ce4b74ccba59feb674a8699e0
Yulia <122625932+Uya13@users.noreply.github.com>
2023-09-07 15:05:09 +0300

    1news scelery dont work (maybe work)(no)

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -9,11 +9,18 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'action_every_thursday_12pm': {
+    'action_every_thursday_at_time': {
         'task': 'news.tasks.weekly_mailing',
-        'schedule': crontab(hour='12', minute='8', day_of_week='thursday'),
+        'schedule': crontab(hour=15, minute=3, day_of_week=4),
     },
 }
+
+# app.conf.beat_schedule = {
+#     'action_every_thursday_12pm': {
+#         'task': 'news.tasks.weekly_mailing',
+#         'schedule': crontab(hour=14, minute=55, day_of_week=4),
+#     },
+# }
 
 # app.conf.beat_schedule = {
 #     'action_every_thursday_12pm': {
